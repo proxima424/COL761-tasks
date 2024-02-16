359081c29d4968288562705421bf5be2e903b6da
MTG <106824746+Mohit-T-Garg@users.noreply.github.com>
2024-02-16 22:42:01 +0530

    Update install.sh

--- A2/install.sh
+++ A2/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone https://github.com/proxima424/COL761-tasks.git
+git clone https://github.com/proxima424/COL761-tasks.git ${pwd}
 cd ./COL761-tasks/A2/
 module load compiler/python/3.6.0/ucs4/gnu/447
 module load pythonpackages/3.6.0/matplotlib/3.0.2/gnu
