e4636de9525def318376d59a700a7a861e34c69b
Vanshita Garg <77899508+VanshitaGarg25@users.noreply.github.com>
2024-02-16 22:51:16 +0530

    Update functions.py

--- A2/Q2/functions.py
+++ A2/Q2/functions.py
@@ -1,8 +1,6 @@
 import os
 import numpy as np
-
-
-def convert_to_gspan_input(input_file, output_path_class0, output_path_class1):
+def convert_to_gspan_input(input_file, output_path):
     """
     Convert the given graph data from the input file to the format required by gSpan.
 
@@ -12,25 +10,22 @@ def convert_to_gspan_input(input_file, output_path_class0, output_path_class1):
     - output_path_class1 (str): Path to save the converted gSpan input for class 1.
 
     Returns:
-    - order (list): List of labels for each graph.
+    - None
     """
-
     order = []
-
     with open(input_file, 'r') as input_file:
         lines = input_file.readlines()
 
-    with open(output_path_class0, 'w') as output_file_class0, open(output_path_class1, 'w') as output_file_class1:
+    with open(output_path, 'w') as output_file:
         current_graph_id = None
 
         for line in lines:
             if line.startswith('#'):
                 current_graph_id = line.split()[1]
                 label = int(line.split()[2])
-                order.append(label)
                 current_graph_id = str(current_graph_id)
-                output_file = output_file_class0 if label == 0 else output_file_class1
                 output_file.write(f't # {current_graph_id}\n')
+                order.append(label)
             elif line.startswith('v'):
                 parts = line.split()
                 node_id = parts[1]
@@ -43,6 +38,7 @@ def convert_to_gspan_input(input_file, output_path_class0, output_path_class1):
                 label = parts[3]
                 output_file.write(f'e {source_id} {target_id} {label}\n')
     return order
+
 
 def run_gspan(input_path, support_threshold):
     """
@@ -59,7 +55,7 @@ def run_gspan(input_path, support_threshold):
     gspan_command = f'./gSpan-64 -f {input_path} -s {support_threshold} -o -i'
     os.system(gspan_command)
 
-def process_subgraph_file(subgraph_file, top_k=50):
+def process_subgraph_file(subgraph_file, top_k = 100):
     """
     Process the subgraph file, select the top-k subgraphs based on support.
 
@@ -84,12 +80,15 @@ def process_subgraph_file(subgraph_file, top_k=50):
 
     if len(sorted_subgraphs) < top_k:
         return sorted_subgraphs
+
     return sorted_subgraphs[:top_k]
 
-def generate_feature_vectors(subgraphs, n):
-    feature_vectors = np.zeros((len(subgraphs), n), dtype=int)  # Initialize a 2D array with zeros
 
-    for i in range(len(subgraphs)):
+def generate_feature_vectors(subgraphs, n):
+    k = len(subgraphs)
+    feature_vectors = np.zeros((k, n), dtype=int)  # Initialize a 2D array with zeros
+
+    for i in range(k):
         # Assuming subgraphs is a list of 50 subgraphs represented as strings
         current_subgraph_str = subgraphs[i]
         
